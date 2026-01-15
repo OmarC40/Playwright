6713e7dcc49048202e468cfe9b33fa5078953a20
OmarCuarenta <50001593+OmarC40@users.noreply.github.com>
2026-01-15 01:15:14 -0600

    casa 14 01

--- tests/basicinteractions.test.ts
+++ tests/basicinteractions.test.ts
@@ -14,7 +14,7 @@ test.skip("Interctions with inputs",async ({page})=>{
 })
 
 test("Sum",async ({page})=>{
-    await await page.goto("https://www.lambdatest.com/selenium-playground/simple-form-demo");
+    await page.goto("https://www.lambdatest.com/selenium-playground/simple-form-demo");
     const sumInput1 = page.locator("(//input[@id='sum1'])[1]")
     const sumInput2 = page.locator("(//input[@id='sum2'])[1]")
     const getValuesBtn = page.locator("(//button[@type='button'])[2]")
@@ -34,5 +34,23 @@ test("Sum",async ({page})=>{
     let expectedResult = num1 + num2;
     console.log(result)
     expect(result).toHaveText("" + expectedResult)
+    /*tenemos dos formas de llenar un formulario con type y fill segun la documentacion de playwright se debe de utilizar fill
+    solo en caso donde se requiera el simular un teclado se utilizara type ya que fill borra todo y llena de golpe el campo
+    mientras que type escribe letra por letra pero si hay algo escrito no lo borrara ocasionando posible error*/
 
 })
+
+test("Check box",async ({page})=>{
+    await page.goto("https://www.testmu.ai/selenium-playground/checkbox-demo/");
+    const singleCheckbox=page.locator("(//input[@type='checkbox'])[1]");
+    expect(singleCheckbox).not.toBeChecked()
+    await singleCheckbox.check()
+    expect(singleCheckbox).toBeChecked()
+
+    
+})
+
+/*notas finales para funciones o validaciones como las de check como vemos no tenemos una forma de validar que no esta seleccionada pero podemos negar por decirlo
+asi la afirmacion con el NOT 
+otro punto a remarcar aqui es que si queremos correr solo una prueba lo que podemos hacer es poner test.only para que corra solo esa pruebba
+otra solucion que hemos utilizado es el skip*/
