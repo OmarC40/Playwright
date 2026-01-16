504cf0090b29c9df4ec4c106a7831cfb64bdf1fc
OmarCuarenta <50001593+OmarC40@users.noreply.github.com>
2026-01-15 23:10:05 -0600

    Casa 15 01

--- tests/frames.test.ts
+++ tests/frames.test.ts
@@ -4,5 +4,35 @@ test("Frames",async ({page})=>{
     await page.goto("https://letcode.in/frame")
     const allframes=page.frames();
     console.log("No of frames "+allframes.length)
- 
+    
+    const frame = page.frameLocator("#firstFr")
+    await frame.locator("input[name='fname']").fill("omar");
+    await frame.locator("input[name='lname']").fill("cuarenta");
+
+    expect(await frame.locator("p.has-text-info").textContent()).toContain("You have entered")
+
+    /*el textcontent lo que hace es extraer todo el texto pero en esta forma ya que no sabemos que va contener el texto o con que se va llenar
+    es por eso que hacemos una validacion parcial con tocontain ya que solo valida parcialmente el texto*/
+
+    const innerFrame = frame.frameLocator("iframe[src='innerFrame']")
+    await innerFrame.locator("input[name='email']").fill("ocuarenta@gmail.com")
+
+    await frame.locator("input[name='fname']").fill("letcode");
+
+    /* Esto es muy inetresante yua que previamente con cypress que es una herramienta que ultilizaba no se podia usar los iframes pero en este caso vemos que
+    lo manejan de una forma mucho mas sencilla y en este caso es posible hasta en el contexto de etrar a iframes que estan adentro de otros  */
+
+    // const myFrame = page.frame("firstFr")
+    //  if (myFrame != null) {
+    //      await myFrame.fill("", "")
+    //  }
+    // await myFrame?.fill("input[name='fname']", "omar")
+    // await myFrame?.fill("input[name='lname']", "cuarenta")
+
+    // expect(await myFrame?.locator("p.has-text-info").textContent()).toContain("You have entered")
+    /*lo que aprendimos aqui con el signo de pregunta es hacer una validacion de si esta nuestro frame por eso nos dice que si bien podemos utlizar
+    un if para la validadcion tambien se puede de esta forma*/
+
+    await page.waitForTimeout(3000);
+
 })
---
+++ tests/windows.test.ts
@@ -0,0 +1,8 @@
+let facebookPage: Page;
+import { expect, Page, test } from "@playwright/test";
+
+test("Interact with multiple tabs", async ({ page }) => {
+
+    await page.goto("https://www.testmu.ai/selenium-playground/window-popup-modal-demo/");
+
+})
