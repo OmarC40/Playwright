674550f1827d48c93e488bae8b54ff06e3fb1787
OmarCuarenta <50001593+OmarC40@users.noreply.github.com>
2026-01-20 01:53:18 -0600

    casa 19 01

---
+++ tests/calendar.test.ts
@@ -0,0 +1,8 @@
+import { expect, Page, test } from "@playwright/test";
+
+test("Calendar", async ({ page }) => {
+
+    await page.goto("https://www.testmu.ai/selenium-playground/window-popup-modal-demo/");
+
+
+})
